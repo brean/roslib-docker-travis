1071ee85ca714abce81601bdd059cf15bbf55acb
Andreas Bresser <self@andreasbresser.de>
2020-04-25 00:49:57 +0200

    working setup for rosbridge

---
+++ volumes/rosbridge/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source "/opt/catkin_ws/devel/setup.bash"
+cd /opt/catkin_ws && catkin build
+source "/opt/catkin_ws/devel/setup.bash"
+cd /opt/catkin_ws/src/rosbridge && roslaunch rosbridge start.launch
